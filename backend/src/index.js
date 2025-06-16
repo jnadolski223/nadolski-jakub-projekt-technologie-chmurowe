755e3dcaa09a663382ac18b5ea2cc7fa9c0dbc58
Jakub Nadolski <j.nadolski223@gmail.com>
2025-06-16 02:17:51 +0200

    Updated endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,161 +1,182 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const validator = require("validator");
-const { v4 } = require("uuid");
+const moment = require("moment");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const PORT = 3000;
 const server = express();
 server.use(express.json());
 server.listen(PORT, () => console.log(`Backend server running at port ${PORT}`));
 
-// ==================== ACCOUNT MANAGEMENT ====================
-const accounts = [];
+// ==================== USERS MANAGEMENT ====================
+const users = [];
 
-server.post("/account", (req, res) => {
-    const { username, firstName, lastName, email, password } = req.body;
+server.post("/users", async (req, res) => {
+    const { username, email, password } = req.body;
 
-    if (!username || !firstName || !lastName || !email || !password) {
+    if (!username || !email || !password) {
         return res.status(400).json({ error: "All fields are required" });
     };
 
-    const isUserPresent = accounts.find(user => user.username === username);
+    const isUserPresent = users.find(user => user.username === username);
     if (isUserPresent) {
-        return res.status(409).json({ error: `User ${username} already exists` });
+        return res.status(409).json({ error: "Requested username is already taken" });
     };
 
     if (!validator.isEmail(email)) {
         return res.status(400).json({ error: "Invalid email format" });
     };
 
-    const newAccount = { username, firstName, lastName, email, password };
-    accounts.push(newAccount);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    res.status(201).json({ message: "New account created" });
+    const newAccount = { userId: uuidv4(), username, email, password: hashedPassword };
+    users.push(newAccount);
+
+    res.status(201).json({ message: "New user created" });
 });
 
-server.get("/accounts", (req, res) => {
-    res.status(200).json(accounts);
+server.get("/users", (req, res) => {
+    res.status(200).json(users);
 });
 
-server.get("/account", (req, res) => {
-    const { username } = req.body;
+server.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
 
-    if (!username) {
-        return res.status(400).json({ error: "Username is required" });
-    };
-
-    const requestedUser = accounts.find(user => user.username === username);
+    const requestedUser = users.find(user => user.userId === userId);
     if (!requestedUser) {
-        return res.status(404).json({ error: `User ${username} not found` });
+        return res.status(404).json({ error: "User not found" });
     };
 
     res.status(200).json(requestedUser);
 });
 
-server.patch("/account", (req, res) => {
-    const { username, firstName, lastName, email, password } = req.body;
-
-    if (!username) {
-        return res.status(400).json({ error: "Username is required" });
-    };
-
-    const requestedUser = accounts.find(user => user.username === username);
+server.patch("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    
+    const requestedUser = users.find(user => user.userId === userId);
     if (!requestedUser) {
-        return res.status(404).json({ error: `User ${username} not found` });
+        return res.status(404).json({ error: "User not found" });
     };
+
+    const { username, email, password } = req.body;
+
+    if (username) {
+        if (requestedUser.username === username) {
+            return res.status(400).json({ error: "Requested username is the same as the current" });
+        };
+
+        const isUserTaken = users.find(user => user.username === username);
+        if (isUserTaken) {
+            return res.status(409).json({ error: "Requested username is already taken" });
+        };
+    }
 
     if (email && !validator.isEmail(email)) {
         return res.status(400).json({ error: "Invalid email format" });
     };
 
-    if (firstName) requestedUser.firstName = firstName;
-    if (lastName) requestedUser.lastName = lastName;
+    if (username) requestedUser.username = username;
     if (email) requestedUser.email = email;
     if (password) requestedUser.password = password;
 
-    res.status(200).json({ message: `User ${username} updated successfully` });
+    res.status(200).json({ message: "User updated" });
 });
 
-server.delete("/account", (req, res) => {
-    const { username } = req.body;
+server.delete("/users/:id", (req, res) => {
+    const userId = req.params.id;
 
-    if (!username) {
-        return res.status(400).json({ error: "Username is required" });
-    };
-
-    const userIndex = accounts.findIndex(user => user.username === username);
+    const userIndex = users.findIndex(user => user.userId === userId);
     if (userIndex === -1) {
-        return res.status(404).json({ error: `User ${username} not found`});
+        return res.status(404).json({ error: "User not found"});
     };
 
-    accounts.splice(userIndex, 1);
-    res.status(200).json({ message: `User ${username} deleted successfully` })
+    users.splice(userIndex, 1);
+    res.status(200).json({ message: "User deleted" });
 });
+// ============================================================
+
+// ===================== LOGIN MANAGEMENT =====================
+server.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const requestedUser = users.find(user => user.username === username);
+    if (!requestedUser) {
+        return res.status(404).json({ error: "User not found" });
+    };
+
+    const isPasswordValid = await bcrypt.compare(password, requestedUser.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ error: "Invalid password" });
+    };
+
+    res.status(200).json({ message: "User logged in" });
+});
+
+// TODO: logout endpoint
+
 // ============================================================
 
 // ===================== EVENT MANAGEMENT =====================
 const events = [];
 
-server.post("/event", (req, res) => {
-    const { username, eventName, date, time, description } = req.body;
+server.post("/events", (req, res) => {
+    const { userId, eventName, date, time, description } = req.body;
 
-    if (!username || !eventName || !date || !time || !description) {
-        return res.status(400).json({ error: "All field are required" });
+    if (!userId || !eventName || !date || !time || !description) {
+        return res.status(400).json({ error: "All fields are required" });
     };
 
-    const isUserPresent = accounts.find(user => user.username == username);
+    const isUserPresent = users.find(user => user.userId === userId);
     if (!isUserPresent) {
-        return res.status(404).json({ error: `User ${username} not found` });
+        return res.status(404).json({ error: "User not found" });
     };
 
-    if (!validator.isDate(date)) {
+    if (!moment(date, "YYYY-MM-DD", true).isValid()) {
         return res.status(400).json({ error: "Invalid date format" });
     };
 
-    if (!validator.isTime(time)) {
+    if (!moment(time, "HH:mm", true).isValid()) {
         return res.status(400).json({ error: "Invalid time format" });
     };
 
-    const newEvent = { eventId: v4(), username, eventName, date, time, description };
+    const newEvent = { eventId: uuidv4(), userId, eventName, date, time, description };
     events.push(newEvent);
-    res.status(201).json({ message: "New event created successfully" });
+    res.status(201).json({ message: "New event created" });
 });
 
 server.get("/events", (req, res) => {
     res.status(200).json(events);
 });
 
-server.get("/event", (req, res) => {
-    const { eventId } = req.body;
-
-    if (!eventId) {
-        return res.status(400).json({ error: "EventId is required" });
-    };
+server.get("/events/:id", (req, res) => {
+    const eventId = req.params.id;
 
     const requestedEvent = events.find(event => event.eventId === eventId);
     if (!requestedEvent) {
-        return res.status(404).json({ error: `Event ${eventId} not found` });
+        return res.status(404).json({ error: "Event not found" });
     };
 
     res.status(200).json(requestedEvent);
 });
 
-server.patch("/event", (req, res) => {
-    const { eventId, eventName, date, time, description } = req.body;
-
-    if (!eventId) {
-        return res.status(400).json({ error: "EventId is required" });
-    };
-
+server.patch("/events/:id", (req, res) => {
+    const eventId = req.params.id;
+    
     const requestedEvent = events.find(event => event.eventId === eventId);
     if (!requestedEvent) {
-        return res.status(404).json({ error: `Event ${eventId} not found` });
+        return res.status(404).json({ error: "Event not found" });
     };
 
-    if (date && !validator.isDate(date)) {
+    const { eventName, date, time, description } = req.body;
+
+    if (date && !moment(date, "YYYY-MM-DD", true).isValid()) {
         return res.status(400).json({ error: "Invalid date format" });
     };
 
-    if (time && !validator.isTime(time)) {
+    if (time && !moment(time, "HH:mm", true).isValid()) {
         return res.status(400).json({ error: "Invalid time format" });
     };
 
@@ -164,23 +185,32 @@ server.patch("/event", (req, res) => {
     if (time) requestedEvent.time = time;
     if(description) requestedEvent.description = description;
 
-    res.status(200).json({ message: `Event ${eventId} updated successfully` });
+    res.status(200).json({ message: "Event updated" });
 });
 
-server.delete("/event", (req, res) => {
-    const { eventId } = req.body;
-
-    if (!eventId) {
-        return res.status(400).json({ error: "EventIs is required" });
-    };
+server.delete("/events/:id", (req, res) => {
+    const eventId = req.params.id;
 
     const eventIndex = events.findIndex(event => event.eventId === eventId);
     if (eventIndex === -1) {
-        return res.status(404).json({ error: `Event ${eventId} not found` });
+        return res.status(404).json({ error: "Event not found" });
     };
 
     events.splice(eventIndex, 1);
-    res.status(200).json({ message: `Event ${eventId} deleted successfully` });
+    res.status(200).json({ message: "Event deleted" });
+});
+
+server.get("/users/:id/events", (req, res) => {
+    const userId = req.params.id;
+
+    const requestedUser = users.find(user => user.userId === userId);
+    if (!requestedUser) {
+        return res.status(404).json({ error: "User not found" });
+    };
+
+    const userEvents = events.filter(event => event.userId === userId);
+
+    res.status(200).json(userEvents);
 });
 
 // ============================================================
